62cdfaa004b79661156a8124b4bf21d90e155b81
Alexandru Istrate <38143225+AlexandruIstrate@users.noreply.github.com>
2021-08-05 14:05:35 +0300

    Set up canvas

--- sketch.js
+++ sketch.js
@@ -1,8 +1,17 @@
+let cols, rows;
+let mazeWidth = 40;
 
 function setup() {
     console.log("Setting up...")
+
+    // Perform setup
+    createCanvas(500, 500);
+    cols = floor(width / mazeWidth);
+    rows = floor(height / mazeWidth);
+
+    console.log("Setup done")
 }
 
 function draw() {
-    
+    background(64);
 }
